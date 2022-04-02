b9b256feb166e8d6a4e585aec682572d57fed66b
Tyler Fanuele <tfanuele@gmail.com>
2022-04-02 16:00:10 -0400

    Added color and formatting

--- snip.py
+++ snip.py
@@ -27,8 +27,12 @@
 #       The program will now, if none of the previous paths are taken, add the
 #           contents of the requested snip to the clipboard for pasting and 
 #           save the previous contents to the save.txt file.
+#
 #   help: displays the help message
+#
+#   add_color: This function adds color and format formatting to a string and returns it
 
+from audioop import add
 from genericpath import exists
 import re
 import sys
@@ -38,31 +42,48 @@ from pathlib import Path
 import os
 from os.path import exists
 
+# color
+G = 32
+R = 31
+B = 34
+#format
+BLD = 1
+UND = 4
+REG = 0
+
+def add_color(wString, color, form):
+    temp1 = "\033[" + str(form) + ";" + str(color) + "m" + wString + "\033[0m"
+    return temp1
+
 # help function
 def help():
-    print("snip.py help:")
-    print("snip.py help => outputs this help menu")
-    print("snip.py list => lists your code snip names")
-    print("snip.py report => reports all of the snip")
-    print("snip.py init => used at the start, initilizes snip files/dir")
-    print("snip.py edit [editor] => allows editing of the snippits.txt file")
-    print("snip.py [snip name] => if the snippit exists in your file, copy to keyboard")
-    print("snip.py return => Puts the previous clipboard buffer back into your clipboard")
-    print("snip.py peek [snip name] => Prints the contents of the snip if it exists.")
+    print(add_color("=== snip.py help:", G, REG))
+    print(add_color("=== snip.py help => outputs this help menu", G, REG))
+    print(add_color("=== snip.py list => lists your code snip names", G, REG))
+    print(add_color("=== snip.py report => reports all of the snip", G, REG))
+    print(add_color("=== snip.py init => used at the start, initilizes snip files/dir", G, REG))
+    print(add_color("=== snip.py edit [editor] => allows editing of the snippits.txt file", G, REG))
+    print(add_color("=== snip.py [snip name] => if the snippit exists in your file, copy to keyboard", G, REG))
+    print(add_color("=== snip.py return => Puts the previous clipboard buffer back into your clipboard", G, REG))
+    print(add_color("=== snip.py peek [snip name] => Prints the contents of the snip if it exists.", G, REG))
 
 def main():
     # print snippy
-    print("             ____\n"+
-          "            / . .\\\n"+
-          "   snippy   \  ---<\n"+
-          "             \  /\n"
-          "   __________/ /\n"
-          "-=:___________/")
-    print("Code Snippets program\n> by Tyler Fanuele.\n")
+    print(add_color("===             ____\n"+
+          "===            / . .\\\n"+
+          "===   snippy   \  ---<\n"+
+          "===             \  /\n"
+          "===   __________/ /\n"
+          "===-=:___________/", G, REG))
+    print(add_color("=== Code Snippets program\n===> by Tyler Fanuele.", G, REG))
+    print(add_color("=== Command issued by user: \n=== ", G, REG), end='')
+    for x in sys.argv:
+        print(add_color(x, B, REG) + " ", end='')
+    print(add_color("\n===", G, REG))
     # Start of program. Defines home dir and wanted snip
     home = str(Path.home())
     if len(sys.argv) - 1  < 1:
-        print("Not enough arguments passed")
+        print("=== Not enough arguments passed")
         return
 
     # wanted is the command or arg given
@@ -74,51 +95,51 @@ def main():
         return
     
     if wanted == "return":
-        print("Starting return sequence...\n")
+        print(add_color("=== Starting return sequence...\n", G, REG))
         sp = open(home + "/.config/snippets/save.txt", "r")
         ret = ""
         for line in sp:
             ret += line
         pyperclip.copy(ret)
         sp.close()
-        print("Your old clipboard item should be in your clipboard again")
-        print("Closing program!")
+        print(add_color("=== Your old clipboard item should be in your clipboard again", G, REG))
+        print(add_color("===  Closing program!", G, REG))
         return
 
     # Init Sequence. Always ends program
     if wanted == "init":
         # if snippits dir exists
-        print("Starting start up script")
+        print(add_color("=== Starting start up script", G, REG))
         if not exists(home + "/.config/snippets"):
-            print("=> Making dir: " + home + "/.config/snippets")
+            print(add_color("=== => Making dir: " + home + "/.config/snippets", G, REG))
             os.system("mkdir " + home + "/.config/snippets")
         else:
-            print("=> snippets directory exists so did nothing...")
+            print(add_color("=== => snippets directory exists so did nothing...", G, BLD))
         # if snippits.txt exists
         if not exists(home + "/.config/snippets/snippets.txt"):
-            print("=> Making file: " + home + "/.config/snippets/snippets.txt")
+            print(add_color("=== => Making file: " + home + "/.config/snippets/snippets.txt", G, REG))
             os.system("touch " + home + "/.config/snippets/snippets.txt")
         else:
-            print("=> snippets.txt file exists so did nothing...")
+            print(add_color("=== => snippets.txt file exists so did nothing...", G, BLD))
         if not exists(home + "/.config/snippets/save.txt"):
-            print("=> Making file: " + home + "/.config/snippets/save.txt")
+            print(add_color("=== => Making file: " + home + "/.config/snippets/save.txt", G, REG))
             os.system("touch " + home + "/.config/snippets/save.txt")
         else:
-            print("=> save.txt file exists so did nothing...")
-        print("\nSetup script finished, closing program!")
+            print(add_color("=== => save.txt file exists so did nothing...", G, BLD))
+        print("\n=== Setup script finished, closing program!")
         return
     
     # Edit sequence. Always ends program
     if wanted == "edit":
         if len(sys.argv) - 1 < 2:
-            print("Not enough args!")
+            print(add_color("=== Not enough args!", R, BLD))
             return
         if not exists(home + "/.config/snippets") or not exists(home + "/.config/snippets/snippets.txt"):
-            print("Snippit dir or snippet.txst file does not exist.\nRun snippet init")
+            print(add_color("=== Snippit dir or snippet.txst file does not exist.\nRun snippet init", R, BLD))
             return
-        print("Attempting to open " + sys.argv[2] + "...\n")
+        print(add_color("=== Attempting to open " + sys.argv[2] + "...\n", G, BLD))
         os.system(sys.argv[2] + " " + home + "/.config/snippets/snippets.txt")
-        print("Closing Program!")
+        print(add_color("=== Closing Program!", G, REG))
         return
 
     fp = open(home + "/.config/snippets/snippets.txt", "r")
@@ -140,40 +161,44 @@ def main():
     # list will list snip names. else do regular op
     # List sequence. Will end program after if statement
     if wanted == "list":
-        print("Printing your snip list:")
+        print(add_color("=== Printing your snip list:", G, REG))
         for string in dict:
-            print("=> " + string)
+            print(add_color("=== => ", G, REG) + add_color(string, B, UND))
     # Report sequence. Will end program after if statement
     elif wanted == "report":
-        print("Reporting your snips:\n")
+        print(add_color("=== Reporting your snips:\n===", G, REG))
         for string in dict:
-            print("=> " + string)
+            print(add_color("=== => ", G, REG) + add_color(string, B, UND))
             for line in dict[string]:
-                print("=> => " + line, end='')
-            print()
+                print(add_color("=== => => ", G, REG) + add_color(line, B, REG), end='')
+            print(add_color("===", G, REG))
     # begin peek sequence. Will end program after if statement
     elif wanted == "peek":
+        if len(sys.argv) - 1 < 2:
+            print(add_color("=== Not enough args!", R, BLD))
+            print(add_color("=== Closing program!", G, REG))
+            return
         wanted = sys.argv[2]
         for i in range(3, len(sys.argv)):
-            print(sys.argv[i])
+            # print(sys.argv[i])
             wanted += " " + sys.argv[i]
-        print("Reporting contents of " + wanted + "...")
+        print(add_color("=== Peeking contents of ", G, REG) + add_color(wanted, B, UND) + add_color("...", G, REG))
         if not wanted in dict:
-            print("That snip does not seem to exist... ")
-            print("Closing program!")
+            print(add_color("=== That snip does not seem to exist... ", R, BLD))
+            print(add_color("=== Closing program!", G, REG))
             return
         for string in dict[wanted]:
-            print("=> " + string)
+            print(add_color("=== => ", G, REG) + add_color(string, B, REG))
     else:
         # Snippit to clipboard sequence
         for i in range(2, len(sys.argv)):
-                print(sys.argv[i])
+                # print(sys.argv[i])
                 wanted += " " + sys.argv[i]
         if not wanted in dict:
-            print("That snip does not seem to exist... ")
-            print("Closing program!")
+            print(add_color("=== That snip does not seem to exist... ", R, BLD))
+            print(add_color("=== Closing program!", G, REG))
             return
-        print("Looking for snippet: " + wanted)
+        print(add_color("=== Looking for snippet: ", G, REG)+ add_color(wanted, B, UND))
         for string in dict[wanted]:
             return_string += string
             sp = open(home + "/.config/snippets/save.txt", "w") # save clipboard
@@ -181,7 +206,7 @@ def main():
             sp.write(save_string)
             sp.close()
             pyperclip.copy(return_string)
-        print("Snip should be in your clipboard")
-    print("Closing program!")
+        print(add_color("=== Snip should be in your clipboard", G, REG))
+    print(add_color("=== Closing program!", G, REG))
     #fp.close()
 main()
