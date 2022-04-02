dd341c7d93663f01ada88390d14b1654b2e3e29d
Tyler Fanuele <tfanuele@gmail.com>
2022-04-02 00:19:12 -0400

    Added more options and renamed python file

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # master-snippets
+
 > By Tyler Fanuele
+
+## What is master snippits?
+
+This is a python program that at a basic level copys code or text to your clipboard for pasting later on. It does this\
+by storing your snippits in a user accesable text file.
+
 ## How to get started
 
-Add the directory to the path.
+Add the directory to the path and make snip.py exicutable with chmod +x snip.py.
+Then run snip.py init to create your config folder and file and run snip.py edit vim to add snips.
+A snip is a refrence to a code snippit. It will be used in the options.
+
+## Options
+
+| cmd | result |
+|-----|--------|
+| snip.py [snip name] | Copys your snip to the clipboard if it exists. |
+| snip.py list | Lists out the names of your snips. |
+| snip.py report | Lists out the names and contents of your snips. |
+| snip.py edit [ editor ] | Allows your to edit your snips file easier. Use your favorite editor. |
+| snip.py help | Prints the help screne to the console. |
+| snip.py init | Creates the config folder and file if they are not created. |
--- snip.py
+++ snip.py
@@ -8,21 +8,41 @@ from pathlib import Path
 import os
 from os.path import exists
 
+def help():
+    print("snip.py help:")
+    print("snip.py help => outputs this help menu")
+    print("snip.py list => lists your code snip names")
+    print("snip.py report => reports all of the snip")
+    print("snip.py init => used at the start, initilizes snip files/dir")
+    print("snip.py edit [editor] => allows editing of the snippits.txt file")
+    print("snip.py [snip name] => if the snippit exists in your file, copy to keyboard")
+
 def main():
-    print("Snippits program\nby Tyler Fanuele.\n")
+    print("Snippits program\n> by Tyler Fanuele.\n")
+    #print("echo " + '"alias snip=' + "'./snippits.py'" +'"' + ">> ~/.bashrc")
     # Start of program. Defines home dir and wanted snip
     home = str(Path.home())
+    if len(sys.argv) - 1  < 1:
+        print("Not enough arguments passed")
+        return
+    # wanted is the command or arg given
     wanted = sys.argv[1]
+    # this is the place our arg is
 
+    if wanted == "help" or wanted == "h":
+        help()
+        return
+
+    # Init Sequence. Always ends program
     if wanted == "init":
-        # if exists(home + "/.config/snippits/snippits.txt") or exists(home + "/.config/snippits"):
+        # if snippits dir exists
         print("Starting start up script")
         if not exists(home + "/.config/snippits"):
             print("=> Making dir: " + home + "/.config/snippits")
             os.system("mkdir " + home + "/.config/snippits")
         else:
             print("=> snippits directory exists so did nothing...")
-
+        # if snippits.txt exists
         if not exists(home + "/.config/snippits/snippits.txt"):
             print("=> Making file: " + home + "/.config/snippits/snippits.txt")
             os.system("touch " + home + "/.config/snippits/snippits.txt")
@@ -30,8 +50,21 @@ def main():
             print("=> snippits.txt file exists so did nothing...")
         print("\nSetup script finished, closing program!")
         return
+    
+    # Edit sequence. Always ends program
     if wanted == "edit":
-        
+        if len(sys.argv) - 1 < 2:
+            print("Not enough args!")
+            return
+        if not exists(home + "/.config/snippits") or not exists(home + "/.config/snippits/snippits.txt"):
+            print("Snippit dir or snippit.txst file does not exist.\nRun snippit init")
+            return
+        print("Attempting to open " + sys.argv[2] + "...\n")
+        os.system(sys.argv[2] + " " + home + "/.config/snippits/snippits.txt")
+        print("Closing Program!")
+        return
+
+    
     home += "/.config/snippits/snippits.txt"
     fp = open(home, "r")
     # init snip dict
@@ -41,19 +74,32 @@ def main():
     top_pattern = r'[\[\]\n]'
     # fill dictionary with lines. [] assigns snip name
     for line in fp:
-        if re.match("[\[][A-Z0-9a-z]*[\]]", line):
+        if re.match("[\[][A-Z0-9a-z ]*[\]]", line):
             current = re.sub(top_pattern, '', line)
         else:
             dict[current].append(line)
     return_string = ""
+
     # looks for 'args'. 
     # list will list snip names. else do regular op
+    # List sequence. Will end program after if statement
     if wanted == "list":
         print("Printing your snip list:")
         for string in dict:
             print("=> " + string)
+    if wanted == "report":
+        print("Reporting your snips:\n")
+        for string in dict:
+            print("=> " + string)
+            for line in dict[string]:
+                print("=> => " + line, end='')
+            print()
     else:
-        if not dict.has_key(wanted):
+        # Snippit to clipboard sequence
+        for i in range(2, len(sys.argv)):
+                print(sys.argv[i])
+                wanted += " " + sys.argv[i]
+        if not wanted in dict:
             print("That snip does not seem to exist... ")
             print("Closing program!")
             return
